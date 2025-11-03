7785d32342740a9879c217a473eade88bec7eb2d
Yash Zade <yashmzade@gmail.com>
2025-11-03 18:19:02 +0530

    remove margin top for proper component display

--- Frontend/src/layouts/default.tsx
+++ Frontend/src/layouts/default.tsx
@@ -1,6 +1,6 @@
+import Navbar from "@/components/navbar";
 import { Link } from "@heroui/link";
 
-import { Navbar } from "@/components/navbar";
 
 export default function DefaultLayout({
   children,
@@ -10,7 +10,7 @@ export default function DefaultLayout({
   return (
     <div className="relative flex flex-col h-screen">
       <Navbar />
-      <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
+      <main className="container mx-auto max-w-7xl px-6 flex-grow">
         {children}
       </main>
       <footer className="w-full flex items-center justify-center py-3">
