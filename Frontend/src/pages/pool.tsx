52c9c3a4656050d37516c96518935947b047e56c
Yash Zade <yashmzade@gmail.com>
2025-11-07 20:42:28 +0530

    Update and add test page

---
+++ Frontend/src/pages/pool.tsx
@@ -0,0 +1,11 @@
+import Pools from "@/components/Pool";
+import { title } from "@/components/primitives";
+import DefaultLayout from "@/layouts/default";
+
+export default function PoolPage() {
+  return (
+    <DefaultLayout>
+      <Pools />
+    </DefaultLayout>
+  );
+}
---
+++ Frontend/src/pages/vault.tsx
@@ -0,0 +1,10 @@
+import Vault from "@/components/Vault";
+import DefaultLayout from "@/layouts/default";
+
+export default function VaultPage() {
+  return (
+    <DefaultLayout>
+      <Vault />
+    </DefaultLayout>
+  );
+}
